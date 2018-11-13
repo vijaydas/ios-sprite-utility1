4f94fa79ca6cd051b96dc4d1ed2f8a8fbb0569bc
Vijay Das <vijaytdas@gmail.com>
2018-11-13 13:09:05 -0600

    add SpriteKit scene and crab animations

--- SpriteUtility/SpriteUtility/ViewController.swift
+++ SpriteUtility/SpriteUtility/ViewController.swift
@@ -7,9 +7,26 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
+    
+    var skscene: CustomScene? = nil
+    
+    @IBOutlet weak var skview: SKView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        skscene = CustomScene(size: view.bounds.size)
+        skview.presentScene(skscene)
+        
+    }
+    
 
+    
+    // skscene is subclass of CustomScene whic 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
